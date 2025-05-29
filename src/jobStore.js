d872dc02ca1a970cd983d8447c8198f57acf6979
4acle <cjnarey@gmail.com>
2025-05-29 14:00:07 -0400

    Passing update job function test

---
+++ src/jobStore.js
@@ -0,0 +1,20 @@
+export class JobStore {
+    constructor() {
+        this.jobs = [];
+    }
+
+    addJob(job) {
+        this.jobs.push(job);
+    }
+    
+    getJobs() {
+        return this.jobs;
+    }
+    
+    updateJob(id, updates) {
+        const job = this.jobs[id];
+        if (job) {
+            Object.assign(job, updates);
+        }
+    }
+}
--- tests/jobStore.test.js
+++ tests/jobStore.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { JobStore } from "./jobStore";
+import { JobStore } from "../src/jobStore";
 
 describe("JobStore", () => {
     it("adds a job to the store", () => {
@@ -13,4 +13,20 @@ describe("JobStore", () => {
         expect(job.length).toBe(1);
         expect(job[0].title).toBe("test");
     });
+
+    it('updates a job in the store', () => {
+        const jobStore = new JobStore();
+        jobStore.addJob({
+            title: "test",
+            company: "test",
+            status: "applied",
+        })
+
+        jobStore.updateJob(0, {
+            status: "interview",
+        })
+
+        const job = jobStore.getJobs();
+        expect(job[0].status).toBe("interview");
+    })
 });
